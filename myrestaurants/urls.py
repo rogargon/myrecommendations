51f0c1d002c23ad8eeb0a6221e072da70a21e8fa
Roberto García <roberto@rhizomik.net>
2017-03-19 23:59:24 +0100

    Update to Django 1.10

--- myrestaurants/urls.py
+++ myrestaurants/urls.py
@@ -3,7 +3,7 @@ from django.utils import timezone
 from django.views.generic import DetailView, ListView, UpdateView
 from models import Restaurant, Dish
 from forms import RestaurantForm, DishForm
-from views import RestaurantCreate, DishCreate, RestaurantDetail
+from views import RestaurantCreate, DishCreate, RestaurantDetail, review
 
 urlpatterns = [
     # List latest 5 restaurants: /myrestaurants/
@@ -26,7 +26,7 @@ urlpatterns = [
             template_name='myrestaurants/dish_detail.html'),
         name='dish_detail'),
 
-# Create a restaurant, /myrestaurants/restaurants/create/
+    # Create a restaurant, /myrestaurants/restaurants/create/
     url(r'^restaurants/create/$',
         RestaurantCreate.as_view(),
         name='restaurant_create'),
@@ -34,9 +34,9 @@ urlpatterns = [
     # Edit restaurant details, ex.: /myrestaurants/restaurants/1/edit/
     url(r'^restaurants/(?P<pk>\d+)/edit/$',
         UpdateView.as_view(
-            model = Restaurant,
-            template_name = 'myrestaurants/form.html',
-            form_class = RestaurantForm),
+            model=Restaurant,
+            template_name='myrestaurants/form.html',
+            form_class=RestaurantForm),
         name='restaurant_edit'),
 
     # Create a restaurant dish, ex.: /myrestaurants/restaurants/1/dishes/create/
@@ -47,13 +47,13 @@ urlpatterns = [
     # Edit restaurant dish details, ex.: /myrestaurants/restaurants/1/dishes/1/edit/
     url(r'^restaurants/(?P<pkr>\d+)/dishes/(?P<pk>\d+)/edit/$',
         UpdateView.as_view(
-            model = Dish,
-            template_name = 'myrestaurants/form.html',
-            form_class = DishForm),
+            model=Dish,
+            template_name='myrestaurants/form.html',
+            form_class=DishForm),
         name='dish_edit'),
 
     # Create a restaurant review, ex.: /myrestaurants/restaurants/1/reviews/create/
     url(r'^restaurants/(?P<pk>\d+)/reviews/create/$',
-        'myrestaurants.views.review',
+        review,
         name='review_create'),
 ]
