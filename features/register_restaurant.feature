61170f2b64ed0e727ce772be71b748177cc9e8c2
Roberto García <roberto@rhizomik.net>
2017-04-15 16:40:08 +0200

    Register Restaurant feature and implementation

---
+++ features/register_restaurant.feature
@@ -0,0 +1,35 @@
+Feature: Register Restaurant
+  In order to keep track of the restaurants I visit
+  As a user
+  I want to register a restaurant together with its location and contact details
+
+  Background: There is a registered user
+    Given Exists a user "user" with password "password"
+
+  Scenario: Register just restaurant name
+    Given I login as user "user" with password "password"
+    When I register restaurant
+      | name        |
+      | The Tavern  |
+    Then I'm viewing the details page for restaurant by "user"
+      | name        |
+      | The Tavern  |
+    And There are 1 restaurants
+
+  Scenario: Register just restaurant name and city
+    Given I login as user "user" with password "password"
+    When I register restaurant
+      | name        | city      | country   |
+      | The Tavern  | London    | England   |
+    Then I'm viewing the details page for restaurant by "user"
+      | name        | city      | country   |
+      | The Tavern  | London    | England   |
+    And There are 1 restaurants
+
+  Scenario: Try to register restaurant but not logged in
+    Given I'm not logged in
+    When I register restaurant
+      | name        |
+      | The Tavern  |
+    Then Server responds with page containing "Server Error (500)"
+    And There are 0 restaurants
---
+++ features/steps/register_restaurant.py
@@ -0,0 +1,39 @@
+from behave import *
+import operator
+from django.db.models import Q
+
+use_step_matcher("parse")
+
+@given('Exists restaurant registered by "{username}"')
+def step_impl(context, username):
+    from django.contrib.auth.models import User
+    user = User.objects.get(username=username)
+    from myrestaurants.models import Restaurant
+    for row in context.table:
+        restaurant = Restaurant(user=user)
+        for heading in row.headings:
+            setattr(restaurant, heading, row[heading])
+        restaurant.save()
+
+@when('I register restaurant')
+def step_impl(context):
+    for row in context.table:
+        context.browser.visit(context.get_url('myrestaurants:restaurant_create'))
+        form = context.browser.find_by_tag('form').first
+        for heading in row.headings:
+            context.browser.fill(heading, row[heading])
+        form.find_by_value('Submit').first.click()
+
+@then('There are {count:n} restaurants')
+def step_impl(context, count):
+    from myrestaurants.models import Restaurant
+    assert count == Restaurant.objects.count()
+
+@then('I\'m viewing the details page for restaurant by "{username}"')
+def step_impl(context, username):
+    q_list = [Q((attribute, context.table.rows[0][attribute])) for attribute in context.table.headings]
+    from django.contrib.auth.models import User
+    q_list.append(Q(('user', User.objects.get(username=username))))
+    from myrestaurants.models import Restaurant
+    restaurant = Restaurant.objects.filter(reduce(operator.and_, q_list)).get()
+    assert context.browser.url == context.get_url(restaurant)
