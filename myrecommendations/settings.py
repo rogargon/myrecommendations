2ac14ceb7712589ee2a0758883190d4308046dea
Roberto García <roberto@rhizomik.net>
2018-04-24 11:21:21 +0200

    Install and configure crispy_forms

--- myrecommendations/settings.py
+++ myrecommendations/settings.py
@@ -40,7 +40,10 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'crispy_forms',
 ]
+
+CRISPY_TEMPLATE_PACK = 'bootstrap4'
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
